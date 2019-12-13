a141c03a87f4b95599209bd5f195bc3eed2cbc27
Mohanarangan Muthukumar <mohan.muthukumar@avalara.com>
2019-12-13 18:22:45 +0530

    Add all the files

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/usrates", function(req, res) {
+  res.sendFile(__dirname + "/resources/rates.json");
+});
+
+app.get("/gst", function(req, res) {
+  res.sendFile(__dirname + "/resources/rates.json");
+});
+
+app.get("/vat", function(req, res) {
+  res.sendFile(__dirname + "/resources/rates.json");
+});
+// app.use(express.static(__dirname + "resources"));
+// app.use("/resources", express.static(__dirname + "/resources"));
+app.listen(3000);
